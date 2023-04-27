03dcc8131eece9c81bf2a3b4b38da10008bd503f
Jason Stewart <jaysin.stewart@gmail.com>
2023-04-27 12:49:21 -0700

    POSTing new toy but has undifined likes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
+/*
+Things to-do for this lab:
+=============================================
+[X] - Fetch new cards
+[X] - Make new cards with proper elements 
+[] - Add a new toy with POST. Make sure it adds to the page without reloading
+[] - Increase a toys likes via the "like button" Do this with PATCH. Make sure it add without reloading the page 
+*/
+
 let addToy = false;
-
-// function createCard (toy) {
-//   let card = document.createElement("div")
-//   card.classList.add("card")
-
-//   let h2 = document.createElement("h2")
-//   h2.textContent = toy.name
-
-//   let img = document.createElement("img")
-//   img.classList.add("toy-avatar")
-//   img.src = toy.image
-
-//   let p = document.createElement("p")
-//   p.textContent = `${toy.likes} Likes`
-
-//   let button = document.createElement("button")
-//   button.textContent = "Like ❤️"
-//   button.classList.add("like-btn")
-//   button.id = toy.id
-
-//   let toyCollection = document.getElementById("toy-collection")
-
-//   toyCollection.appendChild(card)
-//   card.append (h2, img, p, button)
-// }
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -38,11 +22,52 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  fetch("http://localhost:3000/toys") 
-    .then(response => response.json())
-    .then(toys => toys.forEach(toy => createCard(toy)))
-    
-  function createCard (toy) {
+// Event listeners 
+// ============================================
+//Create toy button event listener, form with submit button
+  document.querySelector("form.add-toy-form").addEventListener("submit", handleSubmit)
+
+//Likes button event listener
+  // document.querySelector("")
+//Event Handlers 
+// ============================================
+  function handleSubmit (e) {
+    e.preventDefault()
+
+    let formData = Object.fromEntries(new FormData(e.target))
+
+    renderToyCards(formData)
+    makeNewToy(formData)
+  }
+// Fetches 
+// ============================================
+// Fetch a new toy
+  function getAllToys () {
+    fetch("http://localhost:3000/toys") 
+      .then(response => response.json())
+      .then(toys => toys.forEach(toy => renderToyCards(toy)))
+  }
+  function makeNewToy (formData) {
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+
+      body: JSON.stringify({
+        ...formData,
+        "Likes": 0
+      })
+    })
+      .then(response => response.json())
+      .then(newToy => renderToyCards(newToy))
+  }
+// Render Dom Functions
+// ============================================
+// Make a toy card with required elements
+  function renderToyCards (toy) {
+    // Build toy card
     let card = document.createElement("div")
     card.classList.add("card")
     
@@ -62,8 +87,14 @@ document.addEventListener("DOMContentLoaded", () => {
     button.id = toy.id
     
     let toyCollection = document.getElementById("toy-collection")
-    
+    // Add card to toy collection div
     toyCollection.appendChild(card)
     card.append (h2, img, p, button)
   }
+
+// Initial Render 
+  function initialize () {
+    getAllToys()
+  }
+  initialize()
 });
