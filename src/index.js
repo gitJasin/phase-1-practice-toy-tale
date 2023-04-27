191eb2b150fe6df88ca061d1036360bc786c9c94
Jason Stewart <jaysin.stewart@gmail.com>
2023-04-27 10:33:52 -0700

    Fetch new toys and make toy cards

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 let addToy = false;
 
+// function createCard (toy) {
+//   let card = document.createElement("div")
+//   card.classList.add("card")
+
+//   let h2 = document.createElement("h2")
+//   h2.textContent = toy.name
+
+//   let img = document.createElement("img")
+//   img.classList.add("toy-avatar")
+//   img.src = toy.image
+
+//   let p = document.createElement("p")
+//   p.textContent = `${toy.likes} Likes`
+
+//   let button = document.createElement("button")
+//   button.textContent = "Like ❤️"
+//   button.classList.add("like-btn")
+//   button.id = toy.id
+
+//   let toyCollection = document.getElementById("toy-collection")
+
+//   toyCollection.appendChild(card)
+//   card.append (h2, img, p, button)
+// }
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -12,4 +37,33 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch("http://localhost:3000/toys") 
+    .then(response => response.json())
+    .then(toys => toys.forEach(toy => createCard(toy)))
+    
+  function createCard (toy) {
+    let card = document.createElement("div")
+    card.classList.add("card")
+    
+    let h2 = document.createElement("h2")
+    h2.textContent = toy.name
+    
+    let img = document.createElement("img")
+    img.classList.add("toy-avatar")
+    img.src = toy.image
+    
+    let p = document.createElement("p")
+    p.textContent = `${toy.likes} Likes`
+    
+    let button = document.createElement("button")
+    button.textContent = "Like ❤️"
+    button.classList.add("like-btn")
+    button.id = toy.id
+    
+    let toyCollection = document.getElementById("toy-collection")
+    
+    toyCollection.appendChild(card)
+    card.append (h2, img, p, button)
+  }
 });
